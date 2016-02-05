b763dd4f0a9227d0cf2ef6b3239c25da2e33ceb9
denyskoech <dennis96ist@gmail.com>
2016-02-05 15:18:42 +0300

    Video 1 part 2

--- Part 2 Quick Start/video1.py
+++ Part 2 Quick Start/video1.py
@@ -1,7 +1,9 @@
-#!/usr?bin?python3
+#!/usr/bin/python3
 
-a, b - 0,1
+a,b = 0, 1
 if a < b:
-	print('a ({}) is less than b ({})'.format(a, b))
+    print('a ({}) is less than b ({})'.format(a, b))
 else:
-	print('a ({}) is not less than b ({})'.format(a, b))
+    print('a({}) is not less than b ({})'.format(a, b))
+		
+		
